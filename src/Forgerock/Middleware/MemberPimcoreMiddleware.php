9c5cdf94c0b22b0cf868f57db50b70d3946d5661
Heru Rahmat <heru.akhnuari@salt.co.id>
2023-09-12 13:35:58 +0700

    add member pimcore middleware

---
+++ src/Forgerock/Middleware/MemberPimcoreMiddleware.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Samuelerwardi samuelerwardi@gmail.com
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Lcobucci\JWT\Parser;
+use Illuminate\Http\Response;
+use App\Forgerock\MemberPimcore;
+use Lcobucci\JWT\UnencryptedToken;
+use App\Forgerock\Traits\LoggingTrait;
+use Lcobucci\JWT\Encoding\JoseEncoder;
+use Lcobucci\JWT\Token\InvalidTokenStructure;
+use Lcobucci\JWT\Encoding\CannotDecodeContent;
+use Lcobucci\JWT\Token\UnsupportedHeaderFound;
+
+class MemberPimcoreMiddleware
+{
+    use LoggingTrait;
+
+    /**
+     * Run the request filter.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param string
+     * @return mixed
+     * @throws \Auth0\SDK\Exception\CoreException
+     */
+    public function handle($request, Closure $next)
+    {
+        $responseCode = 401;
+
+        /** TOKEN REQUIRED */
+        if ($request->hasHeader('Authorization')) {
+            $authorization = $request->header('Authorization');
+            $token = null;
+            $authorizationHeader = str_replace('bearer ', '', $authorization);
+            $token = str_replace('Bearer ', '', $authorizationHeader);
+
+            $parser = new Parser();
+
+            try {
+                $parseData = $parser->parse($token);
+            } catch (CannotDecodeContent | InvalidTokenStructure | UnsupportedHeaderFound $e) {
+                echo 'Oh no, an error: ' . $e->getMessage();
+            }
+           
+
+            $memberForgeRockID = $parseData->claims()->get('sub');
+            $memberPimcore = MemberPimcore::Instance($memberForgeRockID);
+            $request->request->add(['memberPimcore' => $memberPimcore]);
+            return $next($request);
+        } else {
+            return response()->json([
+                'status' => false,
+                'code' => "AUTH401",
+                'message' => null,
+                'errorMessage' => [
+                    "token" => [__("message.required")]
+                ],
+                'data' => null
+            ], $responseCode);
+        }
+    }
+
+}
